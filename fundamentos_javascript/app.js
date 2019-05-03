28b48d58e4cebd2d0a0ff3927b2b6a44e53c7f9b
Santos <beatriz.a.santos@accenture.com>
2019-05-03 18:31:36 -0300

    html lendo do javascript

---
+++ fundamentos_javascript/app.js
@@ -0,0 +1,8 @@
+function addPerson() {
+  var cpf = document.getElementById("cpf").value;
+  var nome = document.getElementById("nome").value;
+  var cargo = document.getElementById("cargo").value;
+  var salario = document.getElementById("salario").value;
+  
+  
+}
